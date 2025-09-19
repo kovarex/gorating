24653eeb8b8cd68a72c4feaff398448e803f020f
kovarex <kovarex@gmail.com>
2025-09-19 14:01:24 +0200

    Show first registered and then not registered players.

--- players.php
+++ players.php
@@ -1,15 +1,12 @@
 <?php
 
 echo "<div style=\"text-align:center;\">";
-echo "<form>";
-echo "<input type=\"text\" name=\"search\"/>";
+echo "<form >";
+echo "<input type=\"text\" name=\"search\"/><br/>";
 echo "</form>";
 echo "</div>";
 
 $search = @$_GET["search"];
-if (empty($search))
-  return;
-
 
 function addWithOr($query, $addition)
 {
@@ -23,37 +20,20 @@ $searchQuery = "";
 
 foreach ($parts as $part)
 {
+  if (empty($part))
+    continue;
   if (is_numeric($part) and strlen($part) == 8)
-    $searchQuery = addWithOr($searchQuery, "user.egd_pin=".escape($search));
+    $textQuery = addWithOr($searchQuery, "user.egd_pin=".escape($search));
   else
   {
-    $searchQuery = addWithOr($searchQuery, "MATCH(user.first_name) AGAINST(".escape("*".$part."*")." IN BOOLEAN MODE)");
-    $searchQuery = addWithOr($searchQuery, "MATCH(user.last_name) AGAINST(".escape("*".$part."*")." IN BOOLEAN MODE)");
+    $textQuery = addWithOr($searchQuery, "MATCH(user.first_name) AGAINST(".escape("*".$part."*")." IN BOOLEAN MODE)");
+    $textQuery = addWithOr($searchQuery, "MATCH(user.last_name) AGAINST(".escape("*".$part."*")." IN BOOLEAN MODE)");
   }
 }
 
-$players = query("SELECT
-                    user.id as id,
-                    user.rating as rating,
-                    user.first_name as first_name,
-                    user.last_name as last_name,
-                    user.egd_pin as egd_pin,
-                    user.email as email,
-                    user.register_timestamp as register_timestamp,
-                    admin_level.name as admin_level_name,
-                    inviter.id as inviter_id,
-                    inviter.first_name as inviter_first_name,
-                    inviter.last_name as inviter_last_name,
-                    country.code as country_code
-                  FROM user LEFT JOIN user as inviter ON inviter.id = user.invited_by_user_id,
-                       admin_level,
-                       country
-                  WHERE
-                    user.admin_level_id = admin_level.id and
-                    user.country_id = country.id and
-                    (".$searchQuery.")
-                  ORDER BY user.rating DESC
-                  LIMIT 50");
+if (!empty($textQuery))
+  $searchQuery .= " and (".$textQuery.")";
+
 echo "<table class=\"data-table\">";
 echo "<tr>";
 echo "<th>Rating</th>";
@@ -69,27 +49,59 @@ echo "<th>Registered</th>";
 if (userID())
   echo "<th>Report loss</th>";
 echo "</tr>";
-while($row = $players->fetch_assoc())
+
+
+function showPlayers($searchQuery)
 {
-  echo "<tr>";
-  echo "<td style=\"text-align: right;\">".round($row["rating"])."</td>";
-  echo "<td>".playerLink($row["id"], $row["first_name"]." ".$row["last_name"])."</td>";
-  echo "<td>".egdLink($row["egd_pin"])."</td>";
-  echo "<td style=\"text-align: center;\">".$row["country_code"]."</td>";
-  echo "<td>".$row["admin_level_name"]."</td>";
-  if (canSeeInviters())
-    echo "<td>".playerLink($row["inviter_id"], $row["inviter_first_name"]." ".$row["inviter_last_name"])."</td>";
-  if (canSeeEmails())
-    echo "<td>".$row["email"]."</td>";
-  echo "<td>".(empty($row["register_timestamp"]) ? ("<a href=invites?pin=".$row["egd_pin"].">Invite</a>") : date("d. m. Y H:i", strtotime($row["register_timestamp"])))."</td>";
-  if (userID())
+
+  $players = query("SELECT
+                      user.id as id,
+                      user.rating as rating,
+                      user.first_name as first_name,
+                      user.last_name as last_name,
+                      user.egd_pin as egd_pin,
+                      user.email as email,
+                      user.register_timestamp as register_timestamp,
+                      admin_level.name as admin_level_name,
+                      inviter.id as inviter_id,
+                      inviter.first_name as inviter_first_name,
+                      inviter.last_name as inviter_last_name,
+                      country.code as country_code
+                    FROM user LEFT JOIN user as inviter ON inviter.id = user.invited_by_user_id,
+                         admin_level,
+                         country
+                    WHERE
+                      user.admin_level_id = admin_level.id and
+                      user.country_id = country.id".$searchQuery."
+                    ORDER BY user.rating DESC
+                    LIMIT 50");
+
+  while($row = $players->fetch_assoc())
   {
-    echo "<td>";
-    if ($row["id"] != userID())
-      echo "<a href=\"report?id=".$row["id"]."\"/>Report</a>";
-    echo "</td>";
+    echo "<tr>";
+    echo "<td style=\"text-align: right;\">".round($row["rating"])."</td>";
+    echo "<td>".playerLink($row["id"], $row["first_name"]." ".$row["last_name"])."</td>";
+    echo "<td>".egdLink($row["egd_pin"])."</td>";
+    echo "<td style=\"text-align: center;\">".$row["country_code"]."</td>";
+    echo "<td>".$row["admin_level_name"]."</td>";
+    if (canSeeInviters())
+      echo "<td>".playerLink($row["inviter_id"], $row["inviter_first_name"]." ".$row["inviter_last_name"])."</td>";
+    if (canSeeEmails())
+      echo "<td>".$row["email"]."</td>";
+    echo "<td>".(empty($row["register_timestamp"]) ? ("<a href=invites?pin=".$row["egd_pin"].">Invite</a>") : date("d. m. Y H:i", strtotime($row["register_timestamp"])))."</td>";
+    if (userID())
+    {
+      echo "<td>";
+      if ($row["id"] != userID())
+        echo "<a href=\"report?id=".$row["id"]."\"/>Report</a>";
+      echo "</td>";
+    }
+    echo "</tr>";
   }
-  echo "</tr>";
 }
+
+showPlayers($searchQuery." and user.username is not NULL");
+showPlayers($searchQuery." and user.username is NULL");
+
 echo "</table>";
 ?>
