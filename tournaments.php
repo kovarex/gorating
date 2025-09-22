af4b597a5cc1e6cbe8f93f0001de5ff115aad9cb
kovarex <kovarex@gmail.com>
2025-09-22 12:22:40 +0200

    Tournament table through TableViewer.

--- tournaments.php
+++ tournaments.php
@@ -1,32 +1,40 @@
 <?php
+require("src/table_viewer.php");
 
-$data = query("SELECT
-                 egd_tournament.name as name,
-                 egd_tournament.id as id,
-                 egd_tournament.player_count as player_count,
-                 country.code as country_code,
-                 egd_tournament.timestamp as timestamp,
-                 game_type.name as game_type_name
-               FROM
-                 egd_tournament, country, game_type
-               WHERE
-                 egd_tournament.country_id=country.id and
-                 egd_tournament.game_type_id=game_type.id
-               ORDER BY
-                 egd_tournament.timestamp DESC, egd_tournament.id DESC
-               LIMIT 50");
+$table = new TableViewer("egd_tournament, country, game_type
+                          WHERE
+                            egd_tournament.country_id=country.id and
+                            egd_tournament.game_type_id=game_type.id",
+                         $_GET);
 
-echo "<table class=\"data-table\">";
-echo "<tr><th>Name</th><th>Country</th><th>Players</th><th>Type</th><th>Date</th></tr>";
-while ($row = $data->fetch_assoc())
-{
-  echo "<tr>";
-  echo "<td><a href=\"/tournament?id=".$row["id"]."\">".$row["name"]."</a></td>";
-  echo "<td>".$row["country_code"]."</td>";
-  echo "<td>".$row["player_count"]."</td>";
-  echo "<td>".$row["game_type_name"]."</td>";
-  echo "<td>".date("d. m. Y", strtotime($row["timestamp"]))."</td>";
-  echo "</tr>";
-}
-echo "</table>"
+$table->setPrimarySort(new SortDefinition("timestamp", false));
+$table->setLastSort(new SortDefinition("egd_tournament.id", false));
+
+$table->addColumn("name",
+                  "Name",
+                  array(array("egd_tournament.name", "name"),
+                        array("egd_tournament.id", "id")),
+                  function($row){ echo tournamentLink($row["id"], $row["name"]); });
+
+$table->addColumn("country_code",
+                  "Country",
+                  array(array("country.code", "country_code")),
+                  function($row) { echo $row["country_code"]; });
+
+$table->addColumn("player_count",
+                  "Player count",
+                  array(array("egd_tournament.player_count", "player_count")),
+                  function($row) { echo $row["player_count"]; });
+
+$table->addColumn("game_type_name",
+                  "Class",
+                  array(array("game_type.name", "game_type_name")),
+                  function($row) { echo $row["game_type_name"]; });
+
+$table->addColumn("timestamp",
+                  "Date",
+                  array(array("egd_tournament.timestamp", "timestamp")),
+                  function($row) { echo date("d. m. Y", strtotime($row["timestamp"])); });
+
+$table->render();
 ?>
