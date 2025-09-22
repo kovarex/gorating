5c684624652118ca5608e23a67709ed3cb8490dd
kovarex <kovarex@gmail.com>
2025-09-22 15:59:28 +0200

    Updated about.

--- about.php
+++ about.php
@@ -1,17 +1,17 @@
 <ul>
-  <li>The idea is to have "softcore" unoficial go rating system.</li>
+  <li>The idea is to have "softcore/friendly" unoficial go rating.</li>
   <li>Registered players can play friendly games and report them instantly.</li>
-  <li>This site is not related to EGD directly, it only uses it to identify players and get their initial rating.</li>
+  <li>This site is not related to EGD directly, it uses it for 2 purposes:</li>
+  <li>1) Players initial rating is deduced from EGD when players register.</li>
+  <li>2) Players rating is updated when new tournament with the player is encountered.</li>
   <li>The rating change calculation is the same as on <a href="https://www.europeangodatabase.eu/EGD/EGF_rating_system.php">EGD</a></li>
-  <li>The rating change modifiers on EGD are A=100%, B=75%, C=50% and D=25%. We have 3 categories: Serious:50%, Rapid 25%, Blitz 10%</li>
+  <li>The rating change modifiers on EGD are A=100%, B=75%, C=50% and D=25%. We have 3 categories: Serious=50%, Rapid=25%, Blitz=10%</li>
   <li>People can't just register on this site, you need to be invited by someone who is already registered.</li>
 </ul>
 
 <br/>
 TODO:
 <ul>
-  <li>Update player EGD rating when processing tournaments</li>
-  <li>Update our rating when tournament games are processed (for users which registered before the tournament)</li>
   <li>Country selector</li>
   <li>Some styles</li>
   <li>More functionality for higher admin roles (rating change + log of it in the list of games), removal of games or users, adding sgf files to others etc.</li>
