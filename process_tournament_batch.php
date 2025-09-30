c95fc62dcfabbf9d88ee598fa281bc87e79a980e
kovarex <kovarex@gmail.com>
2025-10-01 00:47:42 +0200

    Print count of remaining tournaments to process when not zero in the script.

--- process_tournament_batch.php
+++ process_tournament_batch.php
@@ -2,6 +2,9 @@
 require_once("src/link_helper.php");
 require_once("src/constants.php");
 require_once("src/process_tournament_helper.php");
+$count = query("SELECT COUNT(*) as count FROM egd_tournament_to_process")->fetch_assoc()["count"];
+if ($count > 0)
+  echo "Remaining tournaments to process:".$count."<br/>\n";
 for ($i = 0; $i < 30; $i++)
 {
   if (!processTournament(NULL))
