3a4200ef81dd961c1390b416efacf3263f97efe6
kovarex <kovarex@gmail.com>
2025-10-01 23:26:06 +0200

    Tool to check if game ratings match, and to find the promotions (automatic or manual) later.

--- collect_manual_rating_changes.php
+++ collect_manual_rating_changes.php
@@ -1,33 +1,54 @@
 <?php
+require_once("src/link_helper.php");
 $userID = @$_GET["id"];
-if (!$userID)
-  return "User id not provided";
-$user = query("SELECT * FROM user WHERE id=".escape($userID))->fetch_assoc();
-if (!$user)
-  return "User with id=\"".$userID."\" doesn't exist.";
+if ($userID)
+{
+  $user = query("SELECT * FROM user WHERE id=".escape($userID))->fetch_assoc();
+  if (!$user)
+    return "User with id=\"".$userID."\" doesn't exist.";
+}
+else
+{
+  $username = @$_GET["username"];
+  if ($username)
+  {
+    $user = query("SELECT * FROM user WHERE username=".escape($username))->fetch_assoc();
+    if (!$user)
+      return "User with username=\"".$username."\" doesn't exist.";
+    $userID = $user["id"];
+  }
+  else
+    return "User id nor username provided";
+}
+
 $games = query("SELECT
-                IF(winner.id=".escape($userID).", game.winner_old_egd_rating, game.loser_old_egd_rating) as old_egd_rating,
-                IF(winner.id=".escape($userID).", game.winner_new_egd_rating, game.loser_new_egd_rating) as new_egd_rating
+                IF(game.winner_user_id=".escape($userID).", game.winner_old_egd_rating, game.loser_old_egd_rating) as old_egd_rating,
+                IF(game.winner_user_id=".escape($userID).", game.winner_new_egd_rating, game.loser_new_egd_rating) as new_egd_rating,
+                egd_tournament.egd_key as egd_tournament_key,
+                game.egd_tournament_round as egd_tournament_round
                FROM
-                 game,
-                 user as winner,
-                 user as loser
+                 game LEFT JOIN egd_tournament ON game.egd_tournament_id=egd_tournament.id
                WHERE
                  game.deleted = false AND
-                 game.winner_user_id = winner.id AND
-                 game.loser_user_id = loser.id AND
-                 (winner_user_id=".escape($_GET["id"])." or loser_user_id=".escape($_GET["id"]).") AND
+                 (game.winner_user_id=".escape($userID)." or game.loser_user_id=".escape($userID).") AND
                  winner_old_egd_rating IS NOT NULL
                ORDER BY
-                 timestamp, egd_tournament_id, egd_tournament_round", true);
+                 game.timestamp, game.egd_tournament_id, game.egd_tournament_round", true);
 while ($row = $games->fetch_assoc())
 {
   $egdOld = $row["old_egd_rating"];
   $egdNew = $row["new_egd_rating"];
   if (isset($egdRating))
   {
+    $tournamentKey = $row["egd_tournament_key"];
     if (abs($egdRating - $egdOld) > 0.01)
-      echo "Rating jumped from ".$egdRating." to ".$egdOld."<br/>\n";
+    {
+      echo "Rating jumped from ".$egdRating." to ".$egdOld." in tournament ".egdTournamentLink($tournamentKey)." round ".$row["egd_tournament_round"];
+      if ($previousTournamentKey and $row["egd_tournament_round"] == 1)
+        echo " (previous tournament ".egdTournamentLink($previousTournamentKey).")";
+      echo "<br/>\n";
+    }
+    $previousTournamentKey = $tournamentKey;
   }
   $egdRating = $egdNew;
 }
