5336fa2f26f40bcab78c7e81e27e6bc776d0f9aa
kovarex <kovarex@gmail.com>
2025-10-13 14:16:01 +0200

    Bump style version.

--- src/header_internal.php
+++ src/header_internal.php
@@ -6,7 +6,7 @@ echo
     <title>
       Friendly go rating
     </title>
-    <link rel=\"stylesheet\" href=\"".$resourceAddress."/style.css\"/>
+    <link rel=\"stylesheet\" href=\"".$resourceAddress."/style2.css\"/>
     <script src=\"".$resourceAddress."/common.js\" type=\"text/javascript\"></script>
   </head>
 <body>";
